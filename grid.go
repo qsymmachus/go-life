df6cccbaeb934266dc3a16d2a1c087563c8ee29a
John Olmsted <john@olmsted.io>
2019-05-26 08:00:42 -0700

    Implements 'RandomGrid()'

--- grid.go
+++ grid.go
@@ -1,6 +1,10 @@
 package life
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type Grid [][]Cell
 
@@ -15,4 +19,29 @@ func (g Grid) String() string {
 	}
 
 	return str
+}
+
+// Generates a grid with the specified width and height, and Cells with random initial state.
+// The 'liveness' parameter determines what % of cells will be alive on the grid.
+func RandomGrid(width, height, liveness int) Grid {
+	var grid Grid
+
+	for y := 0; y < height; y++ {
+		var row []Cell
+		for x := 0; x < width; x++ {
+			row = append(row, Cell{Position{x, y}, randomBool(liveness)})
+		}
+		grid = append(grid, row)
+	}
+
+	return grid
+}
+
+// Returns a boolean that is randomly either true or false.
+// The higher the likelihood percentage, the more likely that the boolean will be true.
+func randomBool(likelihood int) bool {
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+
+	return r.Intn(100) <= likelihood
 }
--- grid_test.go
+++ grid_test.go
@@ -16,3 +16,17 @@ func TestStringGrid(t *testing.T) {
 		t.Errorf("Test grid should print %s; got %s", want, got)
 	}
 }
+
+func TestRandomGrid(t *testing.T) {
+	grid := RandomGrid(5, 5, 30)
+
+	if len(grid) != 5 {
+		t.Errorf("Random grid should have height of 5; got %d", len(grid))
+	}
+
+	for _, row := range grid {
+		if len(row) != 5 {
+			t.Errorf("Random grid should have width of 5; got %d", len(row))
+		}
+	}
+}
