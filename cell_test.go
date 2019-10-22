b062492c53b144cacf3ef7f08adbbe821fecdb81
John Olmsted <john@olmsted.io>
2019-10-22 15:45:36 -0700

    Add function to check if a position is off the grid

--- cell_test.go
+++ cell_test.go
@@ -2,6 +2,24 @@ package main
 
 import "testing"
 
+func TestPositionIsOffGrid(t *testing.T) {
+	positionOnGrid := Position{5, 0}
+	positionXOffGrid := Position{-4, 3}
+	positionYOffGrid := Position{9, -2}
+
+	if positionOnGrid.IsOffGrid() != false {
+		t.Errorf("A position on grid was reported to be off grid")
+	}
+
+	if positionXOffGrid.IsOffGrid() != true {
+		t.Errorf("A position off grid was reported to be on grid")
+	}
+
+	if positionYOffGrid.IsOffGrid() != true {
+		t.Errorf("A position off grid was reported to be on grid")
+	}
+}
+
 func TestCellStringAlive(t *testing.T) {
 	cell := Cell{Alive: true}
 	got := cell.String()
